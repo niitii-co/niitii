7d29573ec38b5ca3e7661e698b0e394934dbc1c4
niitii-co <niitii.co@gmail.com>
2025-04-26 20:58:54 -0400

    translations

--- app/translations/app/app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('api', __name__)
-
-
-
-# bottom imports avoid circular dependency errors
-from app.api import users, errors, tokens
--- app/translations/app/app/main/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-# blueprints define routes and error handlers. Blueprints are dormant until registered with an application.
-bp = Blueprint('main', __name__)
-
-# imported at the bottom to avoid circular dependency. app/main/routes will need to import main blueprint object
-from app.main import routes
-
